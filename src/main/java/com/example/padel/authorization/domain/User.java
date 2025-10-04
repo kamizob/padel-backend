7d796a7a7177d5465024abbd3b167a1675a97728
kamzob <149818908+kamzob@users.noreply.github.com>
2025-10-04 19:32:07 +0300

    Add User domain model and Role enum

--- src/main/java/com/example/padel/authorization/domain/User.java
+++ src/main/java/com/example/padel/authorization/domain/User.java
@@ -1,4 +1,82 @@
 package com.example.padel.authorization.domain;
 
+import com.example.padel.authorization.domain.enums.Role;
+
 public class User {
+    private String id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Role role;
+    private boolean isVerified;
+
+    public User() {}
+
+
+    public User(String id, String email, String password, String firstName, String lastName, Role role, boolean isVerified) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+        this.isVerified = isVerified;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean isVerified() {
+        return isVerified;
+    }
+
+    public void setVerified(boolean verified) {
+        isVerified = verified;
+    }
 }
--- src/main/java/com/example/padel/authorization/domain/enums/Role.java
+++ src/main/java/com/example/padel/authorization/domain/enums/Role.java
@@ -1,4 +1,6 @@
 package com.example.padel.authorization.domain.enums;
 
 public enum Role {
+    ADMIN,
+    USER
 }
